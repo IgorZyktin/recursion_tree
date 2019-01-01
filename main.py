99325463ac93885fc5a41743ca2b123ec783789f
Igor Zyktin <nicord@yandex.ru>
2019-01-02 00:15:48 +0300

    pylint warnings disabled

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
     Main program module
 
 """
+# pylint: disable=C0103
+# pylint: disable=R1710
 import random
 from node import analyze_nodes
 from decorator import recursion_tree, load_nodes_from_json
@@ -16,10 +18,10 @@ def triple(x: int):
     """
     if x == 0:
         return 'Dead end'
-    else:
-        triple(x=x-1)
-        triple(x=x-1)
-        triple(x=x-1)
+
+    triple(x=x-1)
+    triple(x=x-1)
+    triple(x=x-1)
 
 
 @recursion_tree
@@ -29,8 +31,8 @@ def fact(x: int) -> int:
     """
     if x < 2:
         return 1
-    else:
-        return x * fact(x=x-1)
+
+    return x * fact(x=x-1)
 
 
 @recursion_tree
@@ -38,7 +40,7 @@ def fibo(x: int) -> int:
     """
         Fibonacci sequence
     """
-    if x == 1 or x == 2:
+    if x in [1, 2]:
         return 1
 
     return fibo(x=x - 1) + fibo(x=x - 2)
@@ -52,7 +54,7 @@ def random_tree(x: int):
     if x == 0:
         return 'Dead end'
 
-    for i in range(random.randint(1, x)):
+    for _ in range(random.randint(1, x)):
         random_tree(x=x - 1)
 
 
