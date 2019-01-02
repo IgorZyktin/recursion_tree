8e9067ad801548ffe4dc95c3c8ceb8ee8d90f746
Igor Zyktin <nicord@yandex.ru>
2019-01-02 13:30:17 +0300

    recursiontree

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="recursion_tree",
+    name="recursiontree",
     version="1.0",
     author="Igor Zyktin",
     author_email="nicord@yandex.ru",
