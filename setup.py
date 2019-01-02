a4d802c423607466df1cd784a1b71ffd342ecc53
Igor Zyktin <nicord@yandex.ru>
2019-01-02 18:11:14 +0300

    package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="RecursionTree",
-    version="1.4",
+    version="1.5",
     author="Igor Zyktin",
     author_email="nicord@yandex.ru",
     description="Builds recursive calls tree and saves it as a PNG file.",
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/IgorZyktin/recursion_tree",
     packages=find_packages(),
-    package_data={'': ['*.ttf'], },
+    package_data={'': ['RecursionTree/Excalibur-Monospace.ttf']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -22,5 +22,4 @@ setup(
     install_requires=[
         'pillow>=3.4.0',
     ],
-	include_package_data=True, zip_safe=False)
-
+    include_package_data=True, zip_safe=False)
