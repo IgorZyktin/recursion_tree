87d91685cc8728940fc526ef95cca70cee1f35c0
Igor Zyktin <nicord@yandex.ru>
2019-01-02 14:26:29 +0300

    recursiontree

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="recursiontree",
-    version="1.0",
+setup(
+    name="RecursionTree",
+    version="1.2",
     author="Igor Zyktin",
     author_email="nicord@yandex.ru",
     description="Builds recursive calls tree and saves it as a PNG file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IgorZyktin/recursion_tree",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'pillow>=3.4.0',
+    ]
 )
